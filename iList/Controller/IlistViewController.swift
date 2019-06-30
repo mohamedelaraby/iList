1553bbd6414f484995005f149a62e5f5d4c65ce2
Muhammad Alaraby <mohamedelaraby1995@gmail.com>
2019-06-30 17:30:27 +0200

    Link the datasource to the view controller

--- iList/Controller/IlistViewController.swift
+++ iList/Controller/IlistViewController.swift
@@ -12,14 +12,38 @@ import UIKit
 class IlistViewController: UITableViewController {
 
  
-    //Interacting with the model.
+    //Ilist Data source instances.
     var row0Item: IListDataSource
+    var row1Item: IListDataSource
+    var row2Item: IListDataSource
+    var row3Item: IListDataSource
+    var row4Item: IListDataSource
+    var row5Item: IListDataSource
+
     
     //Setup the required initialization. when the view inilized from the storyboard.s
     required init?(coder aDecoder: NSCoder) {
-   row0Item = IListDataSource()
-        row0Item.text = " Take  a jog"
-       super.init(coder: aDecoder)
+        
+        //Init the IList data source instances.
+        row0Item = IListDataSource()
+        row0Item.text = " Take a jog"
+        
+        row1Item = IListDataSource()
+         row1Item.text = " Watch a movie"
+        
+        row2Item = IListDataSource()
+        row2Item.text = "Code an app"
+        
+        row3Item = IListDataSource()
+        row3Item.text = "Walk the Dog"
+        
+        row4Item = IListDataSource()
+        row4Item.text = "Study the design pattern"
+        
+        row5Item = IListDataSource()
+        row5Item.text = " Sleep"
+    
+        super.init(coder: aDecoder)
     }
     
     
@@ -39,19 +63,19 @@ class IlistViewController: UITableViewController {
           if indexPath.row  == 0 {
                 label.text = row0Item.text
             } else if  indexPath.row % 4 == 0 {
-                label.text = " Watch a movie"
+                label.text = row1Item.text
             }  else if indexPath.row % 3 == 0 {
-                label.text = "Code an app"
+                label.text = row2Item.text
             }  else if indexPath.row % 2 == 0 {
-                label.text = "Walk the Dog"
+                label.text = row3Item.text
             }  else if indexPath.row % 1 == 0 {
-                label.text = "Study the design pattern"
+                label.text = row4Item.text
             }
             else {
-                label.text = " Sleep"
+                label.text = row5Item.text
             }
         }
-        
+        //Display new data into the cell.
         configureCheckMark(for: cell, at: indexPath)
         
         return cell
@@ -71,25 +95,62 @@ class IlistViewController: UITableViewController {
        
     }
     
+    
+    //For display new data in the cell because of the recycled cell still hold the old data.
+    
     func configureCheckMark(for cell: UITableViewCell, at indexPath: IndexPath) {
        
-        if indexPath.row == 0 {
-            if row0Item.checkItems {
-                cell.accessoryType = .none
-            } else {
-                cell.accessoryType = .checkmark
-            }
+            if indexPath.row == 0 {
+                if row0Item.checkItems {
+                    cell.accessoryType = .none
+                } else {
+                    cell.accessoryType = .checkmark
+                }
+                
+                row0Item.checkItems = !row0Item.checkItems
+                
+            } else if indexPath.row % 4 == 0 {
             
-            row0Item.checkItems = !row0Item.checkItems
+                if row1Item.checkItems {
+                    cell.accessoryType = .none
+                } else {
+                    cell.accessoryType = .checkmark
+                }
+                
+                row1Item.checkItems = !row1Item.checkItems
+                
+            } else if indexPath.row % 3 == 0 {
+                
+                    if row2Item.checkItems {
+                        cell.accessoryType = .none
+                    } else {
+                        cell.accessoryType = .checkmark
+                    }
+                    
+                    row2Item.checkItems = !row2Item.checkItems
+                    
+                } else if indexPath.row % 2 == 0 {
             
-        } else {
-            if cell.accessoryType == .none {
-                cell.accessoryType = .checkmark
-            } else {
-                cell.accessoryType = .none
-            }
-    }
-        
-   }
-
+                    if row3Item.checkItems {
+                        cell.accessoryType = .none
+                    } else {
+                        cell.accessoryType = .checkmark
+                    }
+                    
+                    row3Item.checkItems = !row3Item.checkItems
+                    
+                } else if indexPath.row % 1 == 0 {
+                
+                    if row4Item.checkItems {
+                        cell.accessoryType = .none
+                    } else {
+                        cell.accessoryType = .checkmark
+                    }
+                    
+                    row4Item.checkItems = !row4Item.checkItems
+               }
+       }
 }
+
+
+
